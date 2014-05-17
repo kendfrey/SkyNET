943e3d93b8319e099321f4509ef5d5666b11b812
Evan Lewis <elewis@westernpaper.com>
2014-05-17 14:47:57 -0600

    Added Kendall's web classes
    Un-modifed as these were good to begin with, just added the namespace
    and cleaned up formatting.

---
+++ SkyNET.Web/Response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace SkyNET.Web
+{
+    public class Response
+    {
+        private HttpWebResponse HttpResponse { get; set; }
+
+        public string Content { get; private set; }
+
+        public Response(HttpWebResponse response)
+        {
+            HttpResponse = response;
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Content = reader.ReadToEnd();
+            }
+        }
+    }
+}
